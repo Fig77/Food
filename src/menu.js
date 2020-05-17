f2017eb6fdb0dff76c999ab0ec9fa1a33fd92792
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-05-17 16:31:51 -0300

    Added placeholder iamges

--- src/menu.js
+++ src/menu.js
@@ -18,11 +18,11 @@ const menu =
      <div class='d-flex flex-column align-i-center menu-card d-none' id='card-1'>
       <h3>Disk with double melted curdled dairy product</h3>
       <div class='d-flex justify-c-center'>
-      <div class='pizza-img'></div>
+      <div class='pizza-img' id='pizza-img-1'></div>
         <ul class='nutritional-facts col-6'>
-          <li>Total Fat 13g</li>
-          <li>Sodium 760mg</li>
-          <li>Protein 13g</li>
+          <li>Total Fat 20g</li>
+          <li>Sodium 1060mg</li>
+          <li>Protein 15g</li>
           <li>Martian refined dust residue 9g</li>
         </ul>
       </div>
@@ -31,12 +31,12 @@ const menu =
   <div class='d-flex flex-column align-i-center menu-card d-none' id='card-2'>
 <h3>Disk with slices of Ananas comosus</h3>
       <div class='d-flex justify-c-center'>
-      <div class='pizza-img'></div>
+      <div class='pizza-img' id='pizza-img-2'></div>
         <ul class='nutritional-facts col-6'>
-          <li>Total Fat 13g</li>
-          <li>Sodium 760mg</li>
-          <li>Protein 13g</li>
-          <li>Martian refined dust residue 9g</li>
+          <li>Total Fat 10g</li>
+          <li>Sodium 7000mg</li>
+          <li>Protein 10g</li>
+          <li>Martian refined dust residue 29g</li>
         </ul>
       </div>
         <span> $ 3.99 US currency. </span>
