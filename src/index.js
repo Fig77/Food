a59e81ed068fe2271257da7e067780edf3273537
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-05-18 15:33:00 -0300

    Took nav and footer from html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash'; // eslint-disable-line
+import setDefault from './navfooter';
 import setAbout from './about';
 import './maincontent';
 import setMenu from './menu';
 import './css/main.css';
 
-
-document.getElementById('menu-link-0').addEventListener('click', setAbout);
-document.getElementById('menu-link-1').addEventListener('click', setMenu);
+window.onload = () => {
+  setDefault();
+  document.getElementById('menu-link-0').addEventListener('click', setAbout);
+  document.getElementById('menu-link-1').addEventListener('click', setMenu);
+};
